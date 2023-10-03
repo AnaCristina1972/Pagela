a64e74ec6eec143529e067c930448d687f55299b
AnaCristina1972 <anac.s.vieira@hotmail.com>
2023-10-02 22:17:30 -0400

    Atualização das telas

---
+++ app/src/main/java/com/example/pagela/Avaliacao_Inicial.java
@@ -0,0 +1,14 @@
+package com.example.pagela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Avaliacao_Inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao_inicial);
+    }
+}
--- app/src/main/java/com/example/pagela/TelaLogin.java
+++ app/src/main/java/com/example/pagela/TelaLogin.java
@@ -2,13 +2,24 @@ package com.example.pagela;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaLogin extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent=new Intent(TelaLogin.this,Avaliacao_Inicial.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
