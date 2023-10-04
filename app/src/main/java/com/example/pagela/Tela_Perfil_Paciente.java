f49e5259b1775415a0e712e5cdb29a7763b510ff
AnaCristina1972 <anac.s.vieira@hotmail.com>
2023-10-04 01:39:16 -0400

    Atualização da  tela Androide manifest, pedindo permissão para uso da camera, criação da tela perfil

--- app/src/main/java/com/example/pagela/Tela_Perfil_Paciente.java
+++ app/src/main/java/com/example/pagela/Tela_Perfil_Paciente.java
@@ -1,14 +1,25 @@
 package com.example.pagela;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Tela_Perfil_Paciente extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil_paciente);
+        Button button = (Button) findViewById(R.id.imageButton);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
     }
 }
